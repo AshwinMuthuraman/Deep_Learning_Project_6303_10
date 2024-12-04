b45af8dcd5a72ffea12b54505d93721dfdbade97
Ashwin <46164303+AshwinMuthuraman@users.noreply.github.com>
2024-12-03 20:40:51 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,4 @@
 
-
-'''
-
-total_step = len(train_loader)
-total_images = len(train_dataset*64)
-print(f"Number of images: {total_images}, Number of batches: {total_step}")
-
-#training stage
-train_losses=[]
-train_accu=[]
-
-val_losses=[]
 val_accu=[]
 
 steps=0
